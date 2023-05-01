b2b9e56e62fa49110491c2ad6f2ea498111a46e5
nlelovic <nenad-lelovic@hotmail.de>
2023-05-01 03:55:36 +0200

    added snowpark and connection

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("HELLO WORLD")
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+
+# Perform query. Caching 10 mins -> ttl=600 and no caching -> ttl=0
+df = conn.query('select top 5 * from SORARE_COMPETITION;', ttl=0)
+
+for row in df.itertuples():
+    st.write(row)
