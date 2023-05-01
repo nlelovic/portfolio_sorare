c3c24a1ddbe8702a7437b8394245fd79a876336e
nlelovic <nenad-lelovic@hotmail.de>
2023-05-01 04:24:42 +0200

    changed connection

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -8,5 +8,4 @@ conn = st.experimental_connection('snowpark')
 # Perform query. Caching 10 mins -> ttl=600 and no caching -> ttl=0
 df = conn.query('select top 5 * from SORARE_COMPETITION;', ttl=0)
 
-for row in df.itertuples():
-    st.write(row)
+st.dataframe(df)
