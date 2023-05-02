49bbdb95c340441cb5cf31dcee17235c75861426
nlelovic <83257197+nlelovic@users.noreply.github.com>
2023-05-02 21:20:15 +0200

    Added one more dependency. Introduced multiple pages to streamlit. Filled out the About page.

---
+++ app/pages/1_About.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.title("About this Project")
+
+st.subheader("What does this project answer? How serious are those calculations and what is it for?")
+
+st.write("""This project was created as an university project to demonstrate newly acquired skills
+         in Machine Learning and Econometrics at Aix-Marseille University. All of the data is as of January 2022.
+         Further it was queried from the Sorare API https://github.com/sorare/api 
+         Thankfully, Sorare provided the data open source. This streamlit app shall represent my skills and be an example for my Data Science Portfolio of Projects. 
+         The underlying database for the deployed app is stored at a Snowflake database, which is not publicly available.
+         """)
+
+st.subheader("Disclaimer")
+
+st.write("""Although the used models were used reasonably and seriously, the implications out of those results
+         are not valid and outdated. The Streamlit app was created for weak informative purposes and
+         never intends to incentivize doing investment decisions based on those results. All statements without guarantee.
+         """)
+
+st.subheader("Acknowledgments")
+
+st.write(""" First, I want to thank Sorare for providing data open source. Without them, the whole Streamlit App and 
+         the university project would not be a great success. So check the Website out at https://sorare.com/ . Further I want to thank University Konstanz and Aix-Marseille University for the high quality education.
+         Special thanks to Ms. Lyudmila Grigoryeva and Mr. Winfried Pohlmeier for drastically improving my relevant hard skills. Last but not least, I want to thank Julia Funkner, Kevin Polaczek, Daniil Parfenov, Jelena Lelovic, and my parents 
+         for all the assistance and happy times in my live. They deserve all the best.
+         """)
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,6 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# page config
+st.set_page_config(
+    page_title="Sorare NFT Cards Exploration and Analysis",
+    #page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 # Initialize connection.
 conn = st.experimental_connection('snowpark')
 
